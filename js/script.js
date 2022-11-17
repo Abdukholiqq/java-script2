81ec85116e66545af07e01ce5c7c86afebe9d0a1
Abdukholiqq <sumusddollar@gmail.com>
2022-11-17 17:58:50 +0500

    homework2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var bilet = 500;
+var mehmonhona = 250;
+var muzey = 120;
+var dollor = 10650.34;
+var yevro = 10650.03;
+var umumiy = ((mehmonhona+bilet)*dollor+(muzey*yevro));
+var pulingiz = prompt('pulingizni kiriting')
+console.log(`umumiy harajatlar ${umumiy} so'm, sizda jami ${pulingiz} so'm pul bor!`)
